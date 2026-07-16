5a2bd5e93dde6f2be8218c907896c541706f885e
Danny <danish161102@gmail.com>
2026-07-17 00:32:47 +0530

    Initial commit: Set up Spring project structure

---
+++ src/main/java/com/danish/xvi/springbasics/Developer.java
@@ -0,0 +1,10 @@
+package com.danish.xvi.springbasics;
+
+// This is a simple POJO (Plain Old Java Object)
+public class Developer {
+
+	// A simple business logic method
+	public void coding() {
+		System.out.println("The developer is writing the code!");
+	}
+}
